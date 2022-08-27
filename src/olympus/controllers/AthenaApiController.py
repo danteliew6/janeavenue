2fee7635ab9ebc813a1f8d4e727e92748e81faa4
danteliew6 <danteliew6@gmail.com>
2022-08-27 14:44:42 +0800

    clean up repository

--- src/olympus/controllers/AthenaApiController.py
+++ src/olympus/controllers/AthenaApiController.py
@@ -6,6 +6,23 @@ from src.olympus import db
 from datetime import datetime
 import json
 import functools
+from src.olympus.ml_models.knn import KNN
+from src.olympus import database
 
 class AthenaApiController():
-    pass
+
+    def classify_company():
+        # Good models: KNN, RF
+        knn = KNN()
+        user_input = request.form.to_dict()
+        classification = knn.classify(user_input,database.get_private_companies())
+        database.add_history(user_input,classification)
+        return jsonify("Predicted Successful") if classification else jsonify("Predicted Unsuccessful")
+
+    def avg_successful_companies_metrics():
+        return jsonify(database.get_avg_successful_company_metrics())
+
+    def avg_unsuccessful_companies_metrics():
+        return jsonify(database.get_avg_unsuccessful_company_metrics())
+
+
--- src/olympus/routes/api_bp.py
+++ src/olympus/routes/api_bp.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from ..controllers.HermesApiController import HermesApiController
+from ..controllers.AthenaApiController import AthenaApiController
 
 
 api_bp = Blueprint("api_bp", __name__)
@@ -8,6 +9,13 @@ api_bp = Blueprint("api_bp", __name__)
 api_bp.route('/get-all-investments', methods=['GET'])(HermesApiController.getAllInvestments)
 api_bp.route('/deposit', methods=['POST'])(HermesApiController.addDeposit)
 api_bp.route('/withdraw', methods=['POST'])(HermesApiController.withdrawDeposit)
+
+api_bp.route('/athena/classify', methods=['POST'])(AthenaApiController.classify_company)
+api_bp.route('/athena/avgsuccessfulmetrics', methods=['POST'])(AthenaApiController.avg_successful_companies_metrics)
+api_bp.route('/athena/avgunsuccessfulmetrics', methods=['POST'])(AthenaApiController.avg_unsuccessful_companies_metrics)
+api_bp.route('/athena/classify', methods=['POST'])(AthenaApiController.classify_company)
+
+
 # api_bp.route('/add-matches', methods=['POST'])(MatchController.addMatches)
 # api_bp.route('/get-team-rankings', methods=['GET'])(MatchController.getTeamRankings)
 # api_bp.route('/delete-competition-data', methods=['DELETE'])(MatchController.deleteCompetitionData)
