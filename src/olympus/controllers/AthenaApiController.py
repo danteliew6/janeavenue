68c5296ec8633c1821c92b5c9dd7e15454d0c0ec
zeph1997 <zeph1997@hotmail.com>
2022-08-28 13:14:01 +0800

    change cors

--- src/olympus/controllers/AthenaApiController.py
+++ src/olympus/controllers/AthenaApiController.py
@@ -69,9 +69,7 @@ class AthenaApiController():
                         pred_next_rounds["Seed"] = 0
         classification["PredictedGrowth"] = pred_next_rounds
         classification["FinalValuation"] = final_valuation
-        result = jsonify(classification)
-        result.headers.add('Access-Control-Allow-Origin', '*')
-        return result
+        return jsonify(classification)
 
     def avg_successful_companies_metrics():
         return jsonify(database.get_avg_successful_company_metrics())
