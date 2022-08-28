06690d56400ca61aeda5211c7b6e37652049224c
danteliew6 <danteliew6@gmail.com>
2022-08-28 11:52:55 +0800

    update cors

--- src/olympus/__init__.py
+++ src/olympus/__init__.py
@@ -7,7 +7,7 @@ import my_secrets
 from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=['*', 'http://localhost:8000'])
 app.config.from_object('config')
 
 def compoundUserInterest(investment, rate):
